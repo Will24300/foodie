6adf80902bb1a9ed89a26875a74613abd47fc6a0
volonte rwicha <volonterwicha2004@gmail.com>
2025-10-19 20:29:16 +0200

    add register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import { Link, Links } from "react-router-dom";
 
 function Register() {
   return (
@@ -8,26 +9,52 @@ function Register() {
         <div className="h-full w-full flex justify-center items-center">
           <h1 className="text-white text-6xl font-bold font-lilita">Foodie</h1>
         </div>
-        <div className="bg-white h-full w-full flex flex-col justify-center items-center">
-          <div>
-            <h2>Hello!</h2>
-            <p>Sign Up to Get Started</p>
+        <div className="bg-white h-full w-full flex flex-col justify-center items-center rounded-tr-4xl relative">
+          <Link to="/">
+            <button className="bg-[#FF9E0C] text-white py-1.5 px-4 rounded cursor-pointer absolute bottom-3 right-3">
+              Back
+            </button>
+          </Link>
+          <div className="w-3/5 mx-auto">
+            <div className="pb-5">
+              <h2 className="font-semibold text-xl">Hello!</h2>
+              <p className="text-sm">Sign Up to Get Started</p>
+            </div>
+            <form className="">
+              <div className="bg-gray-200 py-2 px-2 flex items-center gap-2 rounded mb-3">
+                <FontAwesomeIcon icon={faUser} />
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  className="outline-none w-full"
+                />
+              </div>
+              <div className="bg-gray-200 py-2 px-2 flex items-center gap-2 rounded mb-3">
+                <FontAwesomeIcon icon={faEnvelope} />
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  className="outline-none w-full"
+                />
+              </div>
+              <div className="bg-gray-200 py-2 px-2 flex items-center gap-2 rounded mb-3">
+                <FontAwesomeIcon icon={faLock} />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="outline-none w-full"
+                  required
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="bg-[#FF9E0C] w-full text-white py-1.5 px-4 rounded mt-3 cursor-pointer"
+              >
+                Register
+              </button>
+            </form>
           </div>
-          <form>
-            <div>
-              <FontAwesomeIcon icon={faUser} />
-              <input type="text" placeholder="Full Name" />
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faEnvelope} />
-              <input type="email" placeholder="Email Address" />
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faLock} />
-              <input type="email" placeholder="Email Address" />
-            </div>
-            <button>Register</button>
-          </form>
         </div>
       </section>
     </>
